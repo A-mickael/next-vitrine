a5eee2375aa3d7e8cacc23a05e905a42f1851c1c
A-mika <agniel.mickael@gmail.com>
2025-06-25 11:06:45 +0200

    headers de sécurité

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
 import type { NextConfig } from "next";
 
+const securityHeaders = [
+  {
+    key: "X-Frame-Options", //Empêche l’affichage dans un <iframe> (anti clickjacking)
+    value: "DENY",
+  },
+  {
+    key: "Referrer-Policy", //Limite les infos envoyées lors d’un clic sortant
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "X-Content-Type-Options", //	Empêche certains fichiers d’être mal interprétés par le navigateur
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control", //Accélère la navigation
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security", //Forcer HTTPS sur le site (préchargé par les navigateurs)
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "Permissions-Policy", //Désactive l’accès aux capteurs inutiles (micro, caméra, etc.)
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
